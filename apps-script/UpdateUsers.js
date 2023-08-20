faa9a1fc4038215cca1b2b1c74befdff76918b4c
Andres Marquez <andresdmf55@gmail.com>
2023-08-19 23:39:23 -0500

    Fixed logic and added update users functions to the CRUD

--- apps-script/UpdateUsers.js
+++ apps-script/UpdateUsers.js
@@ -1,24 +1,27 @@
 function editUser(form) {
-    const fila = searchRow(form.userId);
-    
-    sheetsObFallidos.getRange(fila,2,1,sheetsObFallidos.getLastColumn()-1).setValues([[
-      form.userName,
-      form.userLastName,
-      form.userDateOfBirth,
-      form.userPhone,
-      form.userSex,
-      form.userEmail
-    ]])
-  
-    return "User Edited"
-  }
-  
-  function searchRow(id='3'){
-    const ids = sheetsObFallidos.getRange(2,1, sheetsObFallidos.getLastRow()-1, 1).getValues().map(id => id[0])
-  
-    console.log(ids)
-  
-    const index = ids.indexOf(Number(id))
-    const row = index + 2;
-    return row
-  }
+  const fila = searchRow(form.userId);
+
+  sheetsObFallidos.getRange(fila, 2, 1, sheetsObFallidos.getLastColumn() - 1).setValues([[
+    form.userCountry,
+    form.userName,
+    form.userLastName,
+    form.userPhone,
+    form.userDni,
+    form.userCod,
+    form.userReferent,
+    form.userDateOfFirstContact,
+    form.userStatusWeb,
+    form.userProduct,
+    form.userStatusRisk
+  ]])
+
+  return "User Edited"
+}
+
+function searchRow(id = '1') {
+  const ids = sheetsObFallidos.getRange(2, 1, sheetsObFallidos.getLastRow() - 1, 1).getValues().map(id => id[0])
+
+  const index = ids.indexOf(Number(id))
+  const row = index + 2;
+  return row
+}
