cd60ef752fd246f94d8def5e1c1091be45a8b83a
Andres Marquez <andresdmf55@gmail.com>
2023-08-25 08:02:49 -0500

    Added login feature

--- apps-script/Globals.js
+++ apps-script/Globals.js
@@ -8,16 +8,51 @@ const sheetsTipificaciones = SSTIPIFICACIONES.getSheetByName('Tipificaciones')
 
 function validateCredentials(email, password) {
   const sheetAdmins = SpreadsheetApp
-    .openById("")
+    .openById("1t87Zgidm6s4fE5yvO7P77sXoTw8QBLtU9ouCnux_YTM")
     .getSheetByName("Administradores")
 
   const dataSheetAdmins = sheetAdmins.getDataRange().getValues()
 
   const sheetWs = SpreadsheetApp
-    .openById("")
+    .openById("1t87Zgidm6s4fE5yvO7P77sXoTw8QBLtU9ouCnux_YTM")
     .getSheetByName("LoginCrud")
 
   const currentDate = new Date()
 
+  for (let i = 0; i < dataSheetAdmins.length; i++) {
+    if (dataSheetAdmins[i][1] === email && dataSheetAdmins[i][2] === password) {
+      const nameUser = dataSheetAdmins[i][0]
+      const emailUser = dataSheetAdmins[i][1]
 
+      sheetWs.appendRow([
+        `${emailUser}`,
+        nameUser,
+        currentDate.toLocaleDateString(),
+        currentDate.toLocaleTimeString()
+      ])
+
+      return {
+        authorization: "200",
+        name: nameUser,
+        email: emailUser,
+      }
+
+    }
+  }
+
+  return {
+    authorization: "400",
+    name: null,
+    email: null,
+  }
+
+}
+
+function getPageUrl(name) {
+  if (name) {
+    const url = ScriptApp.getService().getUrl()
+    return url + "?page=" + name
+  } else {
+    return ScriptApp.getService().getUrl()
+  }
 }
--- apps-script/ServerFunctions.js
+++ apps-script/ServerFunctions.js
@@ -1,10 +1,81 @@
-function doGet() {
+/*function doGet(e) {
+  if (e.parameter.page) {
+    let pageName = e.parameter.page.trim().toLowerCase()
     const html = HtmlService.createTemplateFromFile('index');
     const output = html.evaluate()
+
+    let htmlOutput = HtmlService.createHtmlOutput(output);
+    htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+      htmlOutput.setXFrameOptionsMode(HtmlService.XFFrameOptionsMode.ALLOWALL) 
+    console.log(e.parameter.page)
     return output
+  } else {
+    const html = HtmlService.createTemplateFromFile('login');
+    const output = html.evaluate()
+
+    let htmlOutput = HtmlService.createHtmlOutput(output);
+    htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+    console.log(e)
   }
-  
-  function include(fileName){
-    return HtmlService.createHtmlOutputFromFile(fileName).getContent()
+
+    if (e.parameter.page) {
+     var pageName = e.parameter.page.trim().toLowerCase();
+     if (pageName !== "login") {
+       var template = HtmlService.createTemplateFromFile(pageName);
+       template.url = getPageUrl();
+       var output = template.evaluate();
+       var htmlOutput = HtmlService.createHtmlOutput(output);
+       htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+       // htmlOutput.setXFrameOptionsMode(HtmlService.XFFrameOptionsMode.ALLOWALL); // Configurar opciones de encabezado X-Frame-Options
+       return htmlOutput;
+     } else {
+       console.log("ndad - nada")
+       return;
+     }
+   } else {
+     console.log("ndad")
+     return;
+   } 
+
+}*/
+
+function doGet(e) {
+  if (e.parameter.page) {
+    var pageName = e.parameter.page.trim().toLowerCase();
+    if (pageName !== "login") {
+      var template = HtmlService.createTemplateFromFile(pageName);
+      template.url = getPageUrl();
+      var output = template.evaluate();
+      var htmlOutput = HtmlService.createHtmlOutput(output);
+      htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+      // htmlOutput.setXFrameOptionsMode(HtmlService.XFFrameOptionsMode.ALLOWALL); // Configurar opciones de encabezado X-Frame-Options
+      return htmlOutput;
+    } else {
+      return homePage();
+    }
+  } else {
+    return homePage();
   }
-  
+}
+
+function homePage() {
+  // var pages = ['vistatabla','vistatabla1','vistatabla2','vistatabla3','vistatabla4'];
+  var pages = ['vistageneral', 'vistatabla', 'vistatabla1', 'vistatabla2', 'vistatabla3', 'vistatabla4', 'vistatabla6', 'vistatabla7', 'vistatabla8'];
+
+  var urls = pages.map(function (name) {
+    return getPageUrl(name);
+  });
+  // var template = HtmlService.createTemplateFromFile("vistageneral");
+  var template = HtmlService.createTemplateFromFile("login");
+
+  template.test = urls;
+  var output = template.evaluate();
+  var htmlOutput = HtmlService.createHtmlOutput(output);
+  htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+
+  return htmlOutput;
+}
+
+function include(fileName) {
+  return HtmlService.createHtmlOutputFromFile(fileName).getContent()
+}
